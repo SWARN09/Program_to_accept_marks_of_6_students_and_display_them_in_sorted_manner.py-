b2df8df1c7bbf947af7c9928c0bee54d783b4904
Suryansh Swarn <118112744+SWARN09@users.noreply.github.com>
2022-11-13 18:43:50 +0530

    Add files via upload

---
+++ Program_to_accept_marks_of_6_students_and_display_them_in_sorted_manner.py
@@ -0,0 +1,14 @@
+#Program_to_accept_marks_of_6_students_and_display_them_in_sorted_manner
+
+s1 = input("Student marks:")
+s2 = input("Student marks:")
+s3 = input("Student marks:")
+s4 = input("Student marks:")
+s5 = input("Student marks:")
+s6 = input("Student marks:")
+
+list = [s1,s2,s3,s4,s5,s6]
+
+list.sort()
+
+print(list)
